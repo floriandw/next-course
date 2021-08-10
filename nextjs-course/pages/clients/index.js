bceef188566a78df9d9b0f1dadf48b25f5abfaa2
Florian De Wulf <florian.dewulf@student.odisee.be>
2021-08-10 12:45:43 +0200

    next project

---
+++ nextjs-course/pages/clients/index.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "flor", name: "Florian" },
+  ];
+  return (
+    <div>
+      <h1>The clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ClientsPage;
